6478119c31bd35a03266d56172fb00afd915319a
Rudhra Deep Biswas <98055396+rudeUltra@users.noreply.github.com>
2026-03-02 01:55:02 +0530

    Refactor AppController to remove AppService dependency

--- server/metro-system/src/app.controller.ts
+++ server/metro-system/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): Promise<number> {
-    return this.appService.getHello();
+  check() {
+    return { status: 'awake' };
   }
 }
